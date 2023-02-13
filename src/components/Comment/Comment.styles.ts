092743d2238b6636a90d341ea94d4dc9ebc9b956
Radik Zangirov <owtmpatrick@gmail.com>
2023-02-13 18:42:44 +0500

    add border

--- src/components/Comment/Comment.styles.ts
+++ src/components/Comment/Comment.styles.ts
@@ -2,13 +2,22 @@ import { Card as MuiCard, styled } from '@mui/material';
 
 export const Card = styled(MuiCard)(({ theme }) => ({
   width: '100%',
-  textAlign: 'left',
 
-  '& .MuiCardHeader-content': {
-    flexGrow: 0
+  '& .MuiCardHeader-root': {
+    position: 'relative',
+
+    '&:before': {
+      content: "''",
+      position: 'absolute',
+      left: '16px',
+      bottom: 0,
+      width: 'calc(100% - 32px)',
+      height: '1px',
+      backgroundColor: theme.palette.grey[300]
+    }
   },
 
   '& .MuiAvatar-root': {
-    background: theme.palette.primary.main
+    backgroundColor: theme.palette.primary.main
   }
 }));
